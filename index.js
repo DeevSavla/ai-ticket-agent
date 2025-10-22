6c84b118a2682a0fa7797f95aa71a6d0052a2c03
Deev Savla <savla.deev@gmail.com>
2025-10-22 11:25:40 +0530

    4. app and db setup

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongodb connected");
+    app.listen(port,()=>{
+        console.log(`Server connected at ${port}.`)
+    })
+  })
+  .catch((error) => console.error("Mongodb Error:", error));
