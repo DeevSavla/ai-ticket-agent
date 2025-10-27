25e6c13e43f12b700ea59ecce3d867eed92299ea
Deev Savla <savla.deev@gmail.com>
2025-10-27 11:31:00 +0530

    7. build ai agent pipeline

---
+++ inngest/functions/on-create.ticket.js
@@ -0,0 +1,83 @@
+import { inngest } from "../client.js";
+import { ticketModel } from "../../models/ticket.model.js";
+import { userModel } from "../../models/user.model.js";
+import { NonRetriableError } from "inngest";
+import { sendMail } from "../../utils/mailer.js";
+import analyzeTicket from "../../utils/ai.js";
+
+export const onTicketCreated = inngest.createFunction(
+  { id: "on-ticket-created", retries: 2 },
+  { event: "ticket/created" },
+  async ({ event, step }) => {
+    try {
+      const { ticketId } = event.data;
+
+      const ticket = await step.run("fetch-ticket", async () => {
+        const ticketObject = await ticketModel.findById(ticketId);
+        if (!ticketObject) {
+          throw new NonRetriableError("Ticket not found");
+        }
+        return ticketObject;
+      });
+
+      await step.run("update-ticket-status", async () => {
+        await ticketModel.findByIdAndUpdate(ticket._id, { status: "TODO" });
+      });
+
+      const aiResponse = await analyzeTicket(ticket);
+
+      const relatedskills = await step.run("ai-processing", async () => {
+        let skills = [];
+        if (aiResponse) {
+          await ticketModel.findByIdAndUpdate(ticket._id, {
+            priority: !["low", "medium", "high"].includes(aiResponse.priority)
+              ? "medium"
+              : aiResponse.priority,
+            helpfulNotes: aiResponse.helpfulNotes,
+            status: "IN_PROGRESS",
+            relatedSkills: aiResponse.relatedSkills,
+          });
+          skills = aiResponse.relatedSkills;
+        }
+        return skills;
+      });
+
+      const moderator = await step.run("assign-moderator", async () => {
+        let user = await userModel.findOne({
+          role: "moderator",
+          skills: {
+            $elemMatch: {
+              $regex: relatedskills.join("|"),
+              $options: "i",
+            },
+          },
+        });
+        if (!user) {
+          user = await userModel.findOne({
+            role: "admin",
+          });
+        }
+        await ticketModel.findByIdAndUpdate(ticket._id, {
+          assignedTo: user?._id || null,
+        });
+        return user;
+      });
+
+      await step.run("send-email-notification", async () => {
+        if (moderator) {
+          const finalTicket = await ticketModel.findById(ticket._id);
+          await sendMail(
+            moderator.email,
+            "Ticket Assigned",
+            `A new ticket is assigned to you ${finalTicket.title}`
+          );
+        }
+      });
+
+      return { success: true };
+    } catch (err) {
+      console.error("Error running the step", err.message);
+      return { success: false };
+    }
+  }
+);
