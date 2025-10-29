12cc74a58dbe9636bddd3dc043191cdea5c367db
Deev Savla <savla.deev@gmail.com>
2025-10-29 11:44:43 +0530

    16. fix bugs

--- ai-ticket-backend/controller/ticket.controller.js
+++ ai-ticket-backend/controller/ticket.controller.js
@@ -76,8 +76,8 @@ export const getTicket = async (req, res) => {
           createdBy: user._id,
           _id: req.params.id,
         })
-        .select("title description status createdAt");
     }
+    console.log(ticket);
     if (!ticket) {
       return res.status(404).json({ message: "Ticket not found" });
     }
